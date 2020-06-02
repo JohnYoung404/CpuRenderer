ca2d2c9b6e0ae0e8ac8abbf42d9feb1346389693
Young John <JohnYoung404@outlook.com>
2020-06-02 22:37:42 +0800

    modify glew/glfw cmakelist

--- CpuRenderer/src/main.cpp
+++ CpuRenderer/src/main.cpp
@@ -7,7 +7,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "My OpenGL Window", NULL, NULL);
 	if (window == NULL)
